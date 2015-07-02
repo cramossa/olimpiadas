ca2352988dcae34f4495d2a4e836aeb03c430db8
cramossa <ramos_s10@hotmail.com>
2015-07-02 13:27:01 +0200

    añadida class,dos sort

--- organizador_personas.py
+++ organizador_personas.py
@@ -7,7 +7,40 @@ import shlex
 from collections import Counter
 
 
-def parse_dataSetOlimpiadas(key,value):
+class ParseDatasetOlimpiadas:
+    def __init__(self):
+        self.registros_erroneos = 0
+        pass
+
+    def __call__(self, key, value):
+
+        tokens0 = value.split(',')
+        splitter = shlex.shlex(value.replace('""', '-'))
+        splitter.whitespace = ','
+        splitter.whitespace_split = True
+        splitter.escapedquotes = '"'
+
+        tokens = list(splitter)
+
+        try:
+            year = tokens[1]
+            pais = tokens[5]
+            disciplina = tokens[3]
+            sexo = tokens[6]
+            nombre = tokens[4]
+
+            year = int(year)
+            medallas = tuple(int(x in tokens[9]) for x in metals)
+
+            yield (nombre, pais), medallas + (year, disciplina)
+
+        except:
+            # yield "Fila erronea", tokens
+            pass
+
+
+
+def parse_dataset_olimpiadas(key, value):
     tokens0 = value.split(',')
     splitter = shlex.shlex(value.replace('""', '-'))
     splitter.whitespace = ','
@@ -26,15 +59,15 @@ def parse_dataSetOlimpiadas(key,value):
         year = int(year)
         medallas = tuple(int(x in tokens[9]) for x in metals)
 
-        yield (nombre , pais), medallas + (year,) + (disciplina,)
+        yield (nombre, pais), medallas + (year, disciplina)
 
     except:
-        #yield "Fila erronea", tokens
+        # yield "Fila erronea", tokens
         pass
 
 
 
-def agrupar_personas_pais(key,values):
+def agrupar_personas_pais(key, values):
 
     years = Counter()
     disciplinas = Counter()
@@ -52,22 +85,53 @@ def agrupar_personas_pais(key,values):
         disciplinas[v[4]] += 1
 
     if(len(years) > 2):
-        yield key ,(oro, plata, bronce, len(years), len(disciplinas))
+        yield key, (oro, plata, bronce, len(years), len(disciplinas))
 
 
-def agrupa_para_sort(key,value):
-    yield " ", (key,) + value
+
+def agrupar_pais(key, values):
+    # pais, (nombre y datos)
+    yield key[1], (key[0], values)
 
 
-def sort_oros(key,values):
-    values = list(values)
-    yield key, sorted(values, key=lambda x: -x[1])
+def sort_tres_mejores(key, values):
+
+    values = sorted(values, key=lambda x: -x[1][0])
+
+    total_oros=0
+
+    for x in values:
+        total_oros += x[1][0]
+
+    for i,x in enumerate(values[0:3]):
+         yield "", (key, total_oros, i+1, x[0]) + x[1]
+
+def agrupa_para_sort(key, values):
+    yield key, values
+
+
+def sort_oros(key, values):
+    cabecera = ["PAIS", "NUM MEDALLAS PAIS","POSICION","PERSONA", "ORO", "PLATA", "BRONCE", "O.DIFERENTES", "D.DIFERENTES"]
+    cabecera = '|'.join(cabecera)
+    yield cabecera,
+    values = sorted(values, key=lambda x: (-x[1], x[2]))
+
+    for x in values:
+        #yield key, x
+        yield "|".join(str(s).replace('"', '') for s in x),
+
+    # si ordeno por los tres mejores de oros y luego por el mejor pais con mas oros, la anterior desaparece
 
 
 if __name__ == "__main__":
     import dumbo
 
     job = dumbo.Job()
-    job.additer(parse_dataSetOlimpiadas, agrupar_personas_pais)
-    job.additer(agrupa_para_sort,sort_oros)
+    job.additer(ParseDatasetOlimpiadas, agrupar_personas_pais)
+    # job.additer(parse_dataset_olimpiadas, agrupar_personas_pais)
+    job.additer(agrupar_pais, sort_tres_mejores)
+
+    opts = [("outputformat", "text"), ]
+
+    job.additer(agrupa_para_sort, sort_oros, opts=opts)
     job.run()
